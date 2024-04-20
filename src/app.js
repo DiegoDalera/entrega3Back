9c7c2eb1cb49127673c17cf019e835b87d989230
DiegoDalera <dmdalera@gmail.com>
2024-04-20 16:05:13 -0300

    version final

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import express from 'express'
-import ProductManager from './ProductManager.class';  
+import ProductManager from './productManager.js';
 
 
 const app = express();
 const PORT = 8080
 
-const ready = ()=> console.log('server ready on port '+ PORT)
+const ready = () => console.log('server ready on port ' + PORT)
 
 // Middleware para parsear JSON.
 app.use(express.json());
 
 // Instanciar ProductManager
-const manager = new ProductManager('./src/products.json'); 
+const manager = new ProductManager('./src/products.json');
 
 //End Points
 app.get('/products', (req, res) => {
@@ -33,9 +33,38 @@ app.get('/products/:pid', (req, res) => {
     }
 });
 
+app.post('/products', async (req, res) => {
+    try {
+        await manager.addProduct(req.body);
+        res.status(201).send('Producto creado con éxito');
+    } catch (error) {
+        res.status(500).send('Error al crear el producto: ' + error.message);
+    }
+});
+
+app.put('/products/:pid', async (req, res) => {
+    const { pid } = req.params;
+    try {
+        await manager.updateProduct(Number(pid), req.body);
+        res.send('Producto actualizado con éxito');
+    } catch (error) {
+        res.status(500).send('Error al actualizar el producto: ' + error.message);
+    }
+});
+
+app.delete('/products/:pid', async (req, res) => {
+    const { pid } = req.params;
+    try {
+        await manager.deleteProduct(Number(pid));
+        res.send('Producto eliminado con éxito');
+    } catch (error) {
+        res.status(500).send('Error al eliminar el producto: ' + error.message);
+    }
+});
 
 
-server.listen(PORT,ready)
+
+app.listen(PORT, ready);
 
 
 
